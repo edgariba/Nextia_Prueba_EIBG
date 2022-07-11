5ea26cb24a5961e425315a7f8b8ddcbbd9227230
Edgar Barrera <edgarlib123@gmail.com>
2022-07-11 14:30:50 -0500

    Initial Commit proyecto generado en start.spring.io

---
+++ src/main/java/com/nextia/PruebaEb/PruebaEbApplication.java
@@ -0,0 +1,13 @@
+package com.nextia.PruebaEb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaEbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaEbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextia/PruebaEb/PruebaEbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextia.PruebaEb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaEbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
