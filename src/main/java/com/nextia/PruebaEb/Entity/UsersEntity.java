d21b9aa7db6896ec95e4bcf9022327675f70e3bf
Edgar Barrera <edgarlib123@gmail.com>
2022-07-11 15:31:25 -0500

    conexión a mysql y entity para usuarios

--- src/main/java/com/nextia/PruebaEb/Entity/UsersEntity.java
+++ src/main/java/com/nextia/PruebaEb/Entity/UsersEntity.java
@@ -1,9 +1,53 @@
 package com.nextia.PruebaEb.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name = "users_entity")
+@Table(name = "t_users")
 public class UsersEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column(name = "hash_user")
+    public String hashUser;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "created_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Column(name = "mofified_date")
+    private Date modifiedDate;
+
+    @Column(name = "role")
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        SuperAdmin
+    }
+
 }
